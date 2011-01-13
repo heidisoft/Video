f161176272b2bb6c7e1495781e3c453ece2d5a38
Heshan <heshan@heidisoft.com>
2011-01-13 15:22:17 +0530

    Updating the module for Drupal 7 version.

---
+++ theme/video-play-flv.tpl.php
@@ -0,0 +1,14 @@
+<?php 
+//$Id: video-play-flv.tpl.php,v 1.1.4.4 2010/12/05 12:56:19 heshanmw Exp $
+/*
+ * @file
+ * Theme file to handle flv output.
+ * 
+ * Variables passed.
+ * $video is the video object.
+ * $node is the node object.
+ * $themed_output is the rendered html.
+ * 
+ */
+print $themed_output;
+?>
